443bf51c9ed5b34bda1e71a9d24050d90c3673c0
joanne342 <130585013+joanne342@users.noreply.github.com>
2026-07-18 11:25:42 +0100

    Update 500.js

--- objects/500.js
+++ objects/500.js
@@ -19,6 +19,10 @@ people.push({
 
 const favouriteFoods = [];
 
+for (let i = 0; i < people.length; i++) {    
+        favouriteFoods.push(people[i].favourite_food);    
+}
+
 // TODO: Fill the array favouriteFoods with the favourite foods of each person.
 // You must not type the strings (i.e. your solution should not involve you typing the word "salad").
 
